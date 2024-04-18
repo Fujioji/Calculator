460285e3dc00587335af8704d02b3e792d9b5bc0
Fujioji <yh_f_tko@yahoo.co.jp>
2024-04-18 23:12:01 +0900

    [add]ログ出力追加

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,9 +12,18 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public Form1()
         {
             InitializeComponent();
+
+            var msg = "起動";
+
+#if DEBUG
+            Console.WriteLine(msg);
+#else
+            _logger.Info(msg);
+#endif
         }
     }
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,6 +16,7 @@ namespace Calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
             Application.Run(new Form1());
         }
     }
